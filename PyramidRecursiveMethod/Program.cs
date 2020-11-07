3adadf458ea5f44018f2414cd6990d9164f74f56
Francis Bulu <Francis.Bulu@student.intecbrussel.be>
2020-11-08 00:07:58 +0100

    Add project files.

---
+++ PyramidRecursiveMethod/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PyramidRecursiveMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Geef een nummer");
+            
+            DrawPyramid(int.Parse(Console.ReadLine()));
+        }
+        static void DrawPyramid(int num)
+        {
+            for (int i = 1; i <= num; i++)
+            {
+                for (int j = i; j <= num; j++)
+                {
+                    Console.Write("  ");
+                }
+                for (int k = 1; k <= 2 * i - 1; k++) // 2 * i - 1
+                {
+                    Console.Write("*" + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
